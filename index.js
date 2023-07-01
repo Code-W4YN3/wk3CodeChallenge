26b02fc61e843c66c706286c97c48aa79418b544
Wayne Otido <otidowayne@gmail.com>
2023-07-01 15:08:57 +0300

    completed challenge

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 addEventListener('DOMContentLoaded', () => {
   listMovies()
+  displayFirst()
 })
 
 function listMovies(){
@@ -13,4 +14,31 @@ function listMovies(){
     movie.innerText = `${film.id}. ${film.title}`
     movieList.appendChild(movie)
   }))
+}
+
+function displayFirst(){
+  fetch('http://localhost:3000/films/1')
+  .then(res => res.json())
+  .then(postMovie)
+  console.log('added first')
+}
+function postMovie(item){
+  document.getElementById('movieImage').setAttribute("src", `${item.poster}`)
+  let AvailableTick = (item.capacity - item.tickets_sold)
+  document.getElementById('movieDetails').innerHTML=`
+  <h4>${item.title}</h4>
+  <p>Showtime: ${item.showtime}</p>
+  <p>Runtime: ${item.runtime} Minutes</p>
+  <p>${item.description}</p>
+  <p> Available Tickets: <span id=tracker>${AvailableTick}</span> </p>
+  <button id="buy"> Buy Ticket </button>
+  `
+  let button = document.getElementById("buy")
+  button.addEventListener('click', tickets)
+  function tickets(){
+    let span =document.getElementById("tracker")
+    console.log(span.innerText)
+    span.innerText = span.innerText-1
+    console.log(span.innerText)
+  }
 }
